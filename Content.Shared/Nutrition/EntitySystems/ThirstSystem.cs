60174a8fc9affb038ef9f7f1d7a87e23da2412e2
A-Mironov <65418470+Alexnov33X@users.noreply.github.com>
2025-11-22 22:10:06 +0300

    q

--- Content.Shared/Nutrition/EntitySystems/ThirstSystem.cs
+++ Content.Shared/Nutrition/EntitySystems/ThirstSystem.cs
@@ -239,25 +239,25 @@ public sealed class ThirstSystem : EntitySystem
 
             ModifyThirst(uid, thirst, -thirst.ActualDecayRate);
             DoContinuousThirstEffects(uid, thirst);
-            
+
             // Fish-Start: Consume extra thirst to heal mangleness
             if (TryComp<DamageableComponent>(uid, out var damageable))
             {
                 if (damageable.Damage.DamageDict.TryGetValue("Mangleness", out var manglenessDamage) && manglenessDamage.Value > 0)
                 {
                     // Consume 1 extra point of thirst per tick to heal 0.05 mangleness
-                    var normalizedConsumption = 1.0f * (float)thirst.UpdateRate.TotalSeconds;
+                    var normalizedConsumption = 0.05f * (float)thirst.UpdateRate.TotalSeconds;
                     if (thirst.CurrentThirst > thirst.ThirstThresholds[ThirstThreshold.Dead] + normalizedConsumption)
                     {
                         ModifyThirst(uid, thirst, -normalizedConsumption);
                         var healAmount = new DamageSpecifier();
-                        healAmount.DamageDict["Mangleness"] = -0.1f * (float)thirst.UpdateRate.TotalSeconds;
+                        healAmount.DamageDict["Mangleness"] = -0.05f * (float)thirst.UpdateRate.TotalSeconds;
                         _damageable.TryChangeDamage(uid, healAmount, true, false);
                     }
                 }
             }
             // Fish-End
-            
+
             var calculatedThirstThreshold = GetThirstThreshold(thirst, thirst.CurrentThirst);
 
             if (calculatedThirstThreshold == thirst.CurrentThirstThreshold)
