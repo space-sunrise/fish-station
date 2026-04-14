c976c02542e994dce2fd37d0fafa420beafcacc5
A-Mironov <65418470+Alexnov33X@users.noreply.github.com>
2026-04-14 15:33:42 +0300

    мини бафф

--- Content.Server/_Fish/Kitsune/KitsuneFoxLightsSystem.cs
+++ Content.Server/_Fish/Kitsune/KitsuneFoxLightsSystem.cs
@@ -66,7 +66,7 @@ namespace Content.Server._Fish.Kitsune
             {
                 DamageDict = new Dictionary<string, FixedPoint2>
                 {
-                    { "Slash", FixedPoint2.New(9) }
+                    { "Slash", FixedPoint2.New(3) }
                 }
             };
             _damageable.TryChangeDamage(uid, damage, ignoreResistances: true);
