61c60d19f1b86feac1ea461a8c5e5047cea6e6a2
A-Mironov <65418470+Alexnov33X@users.noreply.github.com>
2026-06-27 11:03:23 +0300

    Чистка кода

--- Content.Server/_Fish/Weapons/Guns/WeaponTransformSystem.cs
+++ Content.Server/_Fish/Weapons/Guns/WeaponTransformSystem.cs
@@ -3,7 +3,6 @@ using Content.Shared.Interaction;
 using Content.Shared.Actions;
 using Content.Shared.Hands.EntitySystems;
 using Content.Shared.Popups;
-using Content.Shared.Containers.ItemSlots;
 using Robust.Shared.Containers;
 using System.Linq;
 
@@ -19,7 +18,7 @@ public sealed class WeaponTransformSystem : EntitySystem
     public override void Initialize()
     {
         base.Initialize();
-        
+
         SubscribeLocalEvent<WeaponTransformComponent, InteractUsingEvent>(OnInteractUsing);
         SubscribeLocalEvent<WeaponTransformComponent, MapInitEvent>(OnMapInit);
         SubscribeLocalEvent<WeaponTransformComponent, ComponentShutdown>(OnShutdown);
@@ -30,30 +29,25 @@ public sealed class WeaponTransformSystem : EntitySystem
     private void OnGetItemActions(EntityUid uid, WeaponTransformComponent component, ref GetItemActionsEvent args)
     {
         if (component.IsStocked && component.DetachActionEntity != null)
-        {
             args.AddAction(component.DetachActionEntity.Value);
-        }
     }
 
     private void OnMapInit(EntityUid uid, WeaponTransformComponent component, MapInitEvent args)
     {
         if (component.IsStocked && component.DetachAction != null)
-        {
             _actions.AddAction(uid, ref component.DetachActionEntity, component.DetachAction, uid);
-        }
     }
 
     private void OnShutdown(EntityUid uid, WeaponTransformComponent component, ComponentShutdown args)
     {
         if (component.DetachActionEntity != null)
-        {
             _actions.RemoveAction(uid, component.DetachActionEntity);
-        }
     }
 
     private void OnInteractUsing(EntityUid uid, WeaponTransformComponent component, InteractUsingEvent args)
     {
-        if (args.Handled || component.IsStocked || component.TargetPrototype == null || component.StockPrototype == null)
+        if (args.Handled || component.IsStocked || component.TargetPrototype == null ||
+            component.StockPrototype == null)
             return;
 
         if (MetaData(args.Used).EntityPrototype?.ID != component.StockPrototype)
@@ -64,14 +58,14 @@ public sealed class WeaponTransformSystem : EntitySystem
 
         var user = args.User;
         var ammoEntities = ExtractState(uid);
-        
+
         var newGun = Spawn(component.TargetPrototype, Transform(user).Coordinates);
-        
+
         InsertState(newGun, ammoEntities);
 
         _hands.TryDrop(user, args.Used, checkActionBlocker: false);
         _hands.TryDrop(user, uid, checkActionBlocker: false);
-        
+
         Del(args.Used);
         Del(uid);
 
@@ -79,7 +73,9 @@ public sealed class WeaponTransformSystem : EntitySystem
         _popups.PopupEntity(Loc.GetString(component.AttachedPopup), user, user);
     }
 
-    private void OnDetachAction(EntityUid uid, WeaponTransformComponent component, ActionWeaponTransformDetachEvent args)
+    private void OnDetachAction(EntityUid uid,
+        WeaponTransformComponent component,
+        ActionWeaponTransformDetachEvent args)
     {
         args.Handled = true;
 
@@ -99,29 +95,30 @@ public sealed class WeaponTransformSystem : EntitySystem
 
         _hands.TryPickupAnyHand(user, newGun, checkActionBlocker: false);
         _hands.TryPickupAnyHand(user, stock, checkActionBlocker: false);
-        
+
         _popups.PopupEntity(Loc.GetString(component.DetachedPopup), user, user);
     }
 
     private Dictionary<string, EntityUid> ExtractState(EntityUid gun)
     {
         var entities = new Dictionary<string, EntityUid>();
-        if (TryComp<ContainerManagerComponent>(gun, out var containerManager))
+        if (!TryComp<ContainerManagerComponent>(gun, out var containerManager))
+            return entities;
+
+        // Specifically transfer typical gun slots
+        foreach (var (id, container) in containerManager.Containers)
         {
-            // Specifically transfer typical gun slots
-            foreach (var (id, container) in containerManager.Containers)
+            if (id == "gun_magazine" || id == "gun_chamber" || id.StartsWith("item_slot")) // all normal cases are covered, new stuff is not
             {
-                if (id == "gun_magazine" || id == "gun_chamber" || id.StartsWith("item_slot"))
+                if (container.ContainedEntities.Count > 0)
                 {
-                    if (container.ContainedEntities.Count > 0)
-                    {
-                        var item = container.ContainedEntities[0];
-                        _container.Remove(item, container);
-                        entities[id] = item;
-                    }
+                    var item = container.ContainedEntities[0];
+                    _container.Remove(item, container);
+                    entities[id] = item;
                 }
             }
         }
+
         return entities;
     }
 
@@ -136,13 +133,13 @@ public sealed class WeaponTransformSystem : EntitySystem
                     // Clean out pre-existing default items in the new gun first, regardless of whether we are inserting something or not!
                     if (container.ContainedEntities.Count > 0)
                     {
-                        foreach(var oldItem in container.ContainedEntities.ToArray())
+                        foreach (var oldItem in container.ContainedEntities.ToArray())
                         {
                             _container.Remove(oldItem, container);
                             Del(oldItem);
                         }
                     }
-                    
+
                     if (entities.ContainsKey(id))
                     {
                         // Insert preserved item
@@ -152,9 +149,9 @@ public sealed class WeaponTransformSystem : EntitySystem
                 }
             }
         }
-        
+
         // Delete anything remaining that couldn't fit
-        foreach(var ent in entities.Values) 
+        foreach (var ent in entities.Values)
         {
             Del(ent);
         }
